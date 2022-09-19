44a03f3ad5ba174ce24ea4b1fefb3467f9851245
githubBram01 <93133849+githubBram01@users.noreply.github.com>
2022-09-19 12:39:59 +0200

    aanpassing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,13 +21,13 @@ class DatabaseSeeder extends Seeder
             ['id' => 3, 'studentnumber' => 'D284126', 'firstname' => 'Gino', 'lastname' => 'Hanemaaijer'],
             ['id' => 4, 'studentnumber' => 'D280651', 'firstname' => 'Ruben', 'lastname' => 'van Beveren'],
             ['id' => 5, 'studentnumber' => 'D274974', 'firstname' => 'Robin', 'lastname' => 'Aué'],
-            ['id' => 7, 'studentnumber' => 'D232129', 'firstname' => 'Tommy', 'lastname' => 'Aarts'],
-            ['id' => 8, 'studentnumber' => 'D272913', 'firstname' => 'Joey', 'lastname' => 'Belloni'],
-            ['id' => 9, 'studentnumber' => 'D284311', 'firstname' => 'Senne', 'lastname' => 'Loos'],
-            ['id' => 10, 'studentnumber' => 'D282692', 'firstname' => 'Simon', 'lastname' => 'Klaren'],
-            ['id' => 11, 'studentnumber' => 'D283118', 'firstname' => 'Rick', 'lastname' => 'Jansen'],
-            ['id' => 12, 'studentnumber' => 'D284132', 'firstname' => 'Tahar', 'lastname' => 'Grira'],
-            ['id' => 13, 'studentnumber' => 'D281708', 'firstname' => 'Gylian', 'lastname' => 'Crylaards'],
+            ['id' => 6, 'studentnumber' => 'D232129', 'firstname' => 'Tommy', 'lastname' => 'Aarts'],
+            ['id' => 7, 'studentnumber' => 'D272913', 'firstname' => 'Joey', 'lastname' => 'Belloni'],
+            ['id' => 8, 'studentnumber' => 'D284311', 'firstname' => 'Senne', 'lastname' => 'Loos'],
+            ['id' => 9, 'studentnumber' => 'D282692', 'firstname' => 'Simon', 'lastname' => 'Klaren'],
+            ['id' => 10, 'studentnumber' => 'D283118', 'firstname' => 'Rick', 'lastname' => 'Jansen'],
+            ['id' => 11, 'studentnumber' => 'D284132', 'firstname' => 'Tahar', 'lastname' => 'Grira'],
+            ['id' => 12, 'studentnumber' => 'D281708', 'firstname' => 'Gylian', 'lastname' => 'Crylaards'],
         ];
         DB::table('students')->insert($users);
 
