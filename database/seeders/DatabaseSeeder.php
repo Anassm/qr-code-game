b66dba950868914256df85cc9fb54c48ab28028e
githubBram01 <93133849+githubBram01@users.noreply.github.com>
2022-09-19 12:35:01 +0200

    studentsinfo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $users = [
+            ['id' => 1, 'studentnumber' => 'D264663', 'firstname' => 'Bram', 'lastname' => 'van Overveld'],
+            ['id' => 2, 'studentnumber' => 'D284849', 'firstname' => 'Anass', 'lastname' => 'Moussadi'],
+            ['id' => 3, 'studentnumber' => 'D284126', 'firstname' => 'Gino', 'lastname' => 'Hanemaaijer'],
+            ['id' => 4, 'studentnumber' => 'D280651', 'firstname' => 'Ruben', 'lastname' => 'van Beveren'],
+            ['id' => 5, 'studentnumber' => 'D274974', 'firstname' => 'Robin', 'lastname' => 'Aué'],
+            ['id' => 7, 'studentnumber' => 'D232129', 'firstname' => 'Tommy', 'lastname' => 'Aarts'],
+            ['id' => 8, 'studentnumber' => 'D272913', 'firstname' => 'Joey', 'lastname' => 'Belloni'],
+            ['id' => 9, 'studentnumber' => 'D284311', 'firstname' => 'Senne', 'lastname' => 'Loos'],
+            ['id' => 10, 'studentnumber' => 'D282692', 'firstname' => 'Simon', 'lastname' => 'Klaren'],
+            ['id' => 11, 'studentnumber' => 'D283118', 'firstname' => 'Rick', 'lastname' => 'Jansen'],
+            ['id' => 12, 'studentnumber' => 'D284132', 'firstname' => 'Tahar', 'lastname' => 'Grira'],
+            ['id' => 13, 'studentnumber' => 'D281708', 'firstname' => 'Gylian', 'lastname' => 'Crylaards'],
+        ];
+        DB::table('students')->insert($users);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -2,18 +2,13 @@
 @section('app')
     <div class="title">De QR-Code-Game!</div>
     <div class="qr-code-grid">
-        <div class="qrcode">
-            <img src="data:image/png;base64, {{ base64_encode(QrCode::format('png')->size(250)->eye('square')->color(40, 167, 69)->generate($url)) }}" alt="QR-code">
-        </div>
-        </div>
-        <div class="qrcode">
-            dsfll
-        </div>
-        <div class="qrcode">
-            dsfll
-        </div>        <div class="qrcode">
-            dsfll
-        </div>
-
+        @foreach()
+            <div class="qrcode">
+                @php
+                    $url = "https://qr-code-game.dev/$studentnr";
+                @endphp
+                {{ QrCode::generate($url) }}
+            </div>
+        @endforeach
     </div>
 @endsection
