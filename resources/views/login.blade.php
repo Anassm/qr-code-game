1e90d88b9f7b84e4fccd4e025ad48a94321efb05
Anass Moussadi <a.moussadi03@gmail.com>
2022-09-20 14:43:01 +0200

    play pagina qr codes + styling

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,8 @@
 @extends('base')
 @section('app')
+    <h1 class="login-title">Log in met jouw studenten-nummer</h1>
     <div class="login">
-        <input type="text" name="" id="" >
-        <a href="/play" class="play-btn"></a>
+        <input type="text" class="d-nummer" name="studenten-nummer" placeholder="Bijv. D696969">
+        <a href="/play" class="login-btn">Login</a>
     </div>
 @endsection
--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -1,11 +1,14 @@
 @extends('base')
 @section('app')
-    <div class="title">De QR-Code-Game!</div>
-    <div class="qr-code-grid">
-        @foreach($students as $student)
-            <div class="qrcode">
-                {{ QrCode::generate("https://qr-code-game.dev/$student->studentnumber") }}
-            </div>
-        @endforeach
+<div class="title">De QR-Code-Game!</div>
+<div class="qr-code-grid">
+    @foreach($students as $student)
+    <div class="studenten">
+    <style>
+        .studenten::after {content: "{{$student->firstname}}";}
+    </style>
+        {{ QrCode::generate("https://qr-code-game.dev/$student->studentnumber") }}
     </div>
+    @endforeach
+</div>
 @endsection
