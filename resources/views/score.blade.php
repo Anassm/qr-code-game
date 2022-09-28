fed5c6c45b9dab4e4d76485e072dc4cfb5ceaa63
belrace <60099664+belrace@users.noreply.github.com>
2022-09-28 13:28:05 +0200

    front end score

--- resources/views/score.blade.php
+++ resources/views/score.blade.php
@@ -1,1 +1,27 @@
-dsfasadfsdafsdafdsa
+@extends('base')
+@section('app')
+
+
+<style>
+html { background: ;   
+  display:flex;
+    justify-content: center;
+    align-items: center;}
+section {
+  background: ;
+  color: black;
+
+
+  }
+</style>
+
+<section>
+  <h1 style="text-align:center"> je score is </h1>
+  <h1 style="text-align:center">/student naam\ </h1>
+
+</section>
+
+
+
+
+@endsection
